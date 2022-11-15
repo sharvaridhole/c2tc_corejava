e890ed598ccc7454b826b5c228b36ff700b05148
INTEL <INTEL@windows10>
2022-11-15 18:44:21 +0530

    First Commit

---
+++ HelloWorld/src/ForLoop.java
@@ -0,0 +1,17 @@
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+        for(int i=1; i<=10; i++)
+        {
+       	 if(i == 1)
+       	 {
+       		 continue;   // break;
+       	 }
+       	 System.out.println(i);
+        }
+	  
+
+	}
+
+}
